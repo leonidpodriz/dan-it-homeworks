06e097f1194816c99e832e7a714e920745efd417
Leonid Podriz <leonidpodriz@gmail.com>
2021-08-02 19:28:10 +0300

    Initial commit

---
+++ src/main/java/com/danit/springhomeworks/SpringHomeworksApplication.java
@@ -0,0 +1,13 @@
+package com.danit.springhomeworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHomeworksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHomeworksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/danit/springhomeworks/SpringHomeworksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danit.springhomeworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHomeworksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
