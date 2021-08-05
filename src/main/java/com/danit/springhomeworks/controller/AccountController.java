1625b0f54a33d0deed6035739373b7aaaca6807d
Leonid Podriz <leonidpodriz@gmail.com>
2021-08-05 21:29:33 +0300

    Refactor transfer method

--- src/main/java/com/danit/springhomeworks/controller/AccountController.java
+++ src/main/java/com/danit/springhomeworks/controller/AccountController.java
@@ -1,32 +1,42 @@
 package com.danit.springhomeworks.controller;
 
 import com.danit.springhomeworks.dao.AccountDao;
+import com.danit.springhomeworks.entity.Account;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class AccountController {
-//    Пополнить счет (принимает номер счета и сумму)
-//    Снять деньги со счета (принимает номер счета и сумму, выполняется только если на счету достаточно денег)
-//    Перевести деньги на другой счет (принимает два номера счета и сумму, выполняется только если на счету достаточно денег)
-
     private final AccountDao accountDao;
 
     public AccountController() {
         this.accountDao = new AccountDao();
     }
 
-//    public void replenish(String number, Double amount) {
-//        Account account = accountDao.getOneByNumber(number);
-//        account.setBalance(account.getBalance() + amount);
-//    }
-//
-//    public void withdraw(String number, Double amount) {
-//        Account account = accountDao.getOneByNumber(number);
-//        account.setBalance(account.getBalance() - amount);
-//    }
-//
-//    public void transfer(String numberSource, String numberTarget, Double amount) {
-//        withdraw(numberSource, amount);
-//        replenish(numberTarget, amount);
-//    }
+    public void replenish(Account account, Double amount) {
+        account.setBalance(account.getBalance() + amount);
+    }
+
+    public void withdraw(Account account, Double amount) {
+        account.setBalance(account.getBalance() - amount);
+    }
+
+    public void transfer(Account accountFrom, Account accountTo, Double amount) {
+        withdraw(accountFrom, amount);
+        replenish(accountTo, amount);
+    }
+
+    public boolean transfer(String accountNumberFrom, String accountNumberTo, Double amount) {
+        Optional<Account> accountFrom = accountDao.getOneByNumber(accountNumberFrom);
+        Optional<Account> accountTo = accountDao.getOneByNumber(accountNumberTo);
+
+        if (accountFrom.isPresent() && accountTo.isPresent()) {
+            transfer(accountFrom.get(), accountTo.get(), amount);
+
+            return true;
+        }
+
+        return false;
+    }
 }
